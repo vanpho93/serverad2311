a7b7be1bdae6eafefe2dd1c12dfa57ca21cd20f3
Admin <vanpho02@gmail.com>
2016-12-19 08:21:08 +0700

    start

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(3000, () => console.log('Server started'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/admin', (req, res) => res.render('admin'));
+
+io.on('connection', socket => {
+  console.log('Co nguoi ket noi');
+});
